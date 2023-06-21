a0447164b13cbd23ecd7965b4025733276d916b7
rafijadika <jadika.rafi@gmail.com>
2023-06-21 15:31:56 +0700

    commit 12

--- StreamReaderWriter_007/StreamReaderWriter_007.cpp
+++ StreamReaderWriter_007/StreamReaderWriter_007.cpp
@@ -18,6 +18,19 @@ int main() {
 	}
 	outfile.close();
 
-	
+	ifstream infile;
+	infile.open("contohfile.txt");
+
+	cout << endl << ">= Membuka dan Membaca file " << endl;
+	if (infile.is_open())
+	{
+		while (getline(infile, baris))
+		{
+			cout << baris << '\n';
+		}
+		infile.close();
+	}
+	else cout << "unable to open file";
+	return 0;
 
 } 
