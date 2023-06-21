f4f5c1d78011c475e317579296ffe45aaa66c4db
rafijadika <jadika.rafi@gmail.com>
2023-06-21 14:50:34 +0700

    commit 2

--- StreamReaderWriter_007/StreamReaderWriter_007.cpp
+++ StreamReaderWriter_007/StreamReaderWriter_007.cpp
@@ -6,5 +6,15 @@ using namespace std;
 int main() {
 	string baris;
 	ofstream outfile;
-	outfile.open("contohfile.txt")
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	while (true) {
+		cout << "--";
+		getline(cin, baris);
+		if (baris == "q") break;
+		outfile << baris << endl;
+	}
+
 } 
