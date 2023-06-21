ee367fda5ce98043b92e6b5f4d9a5e3f279ecfe4
rafijadika <jadika.rafi@gmail.com>
2023-06-21 15:17:11 +0700

    commit 8

--- iosfilewithpath/iosfilewithpath.cpp
+++ iosfilewithpath/iosfilewithpath.cpp
@@ -7,5 +7,13 @@ int main() {
 	string baris;
 	string NamaFile;
 
+	cout << "masuikan nama file : ";
+	cin >> NamaFile;
+
+	ofstream outfile;
+	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << ">= menulis file, \'q\' untuk keluar" << endl;
+
 	
 }
